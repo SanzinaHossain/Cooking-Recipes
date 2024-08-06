abde9ed3d10b6f78c58ef56f03c4a1a273d8ae33
SanzinaHossain <sanzina67hossain@gmail.com>
2024-08-06 23:54:38 +0600

    firebase enviroment fiile create

--- firebase.init.js
+++ firebase.init.js
@@ -1,19 +1,26 @@
 import { initializeApp } from "firebase/app"
-import { getAnalytics } from "firebase/analytics"
-import { getAuth, initializeAuth } from "firebase/auth"
+import { getAuth } from "firebase/auth"
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  MEASUREMENT_ID,
+} from "@env"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC6ByLRCMnNPPetKm_ndakYAqoDMUqpx7c",
-  authDomain: "cooking-recipes-81f7c.firebaseapp.com",
-  projectId: "cooking-recipes-81f7c",
-  storageBucket: "cooking-recipes-81f7c.appspot.com",
-  messagingSenderId: "342507692855",
-  appId: "1:342507692855:web:fb4cfdd4f09b76f03570b2",
-  measurementId: "G-8C7PL3VH5M",
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
 }
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 
 export const auth = getAuth(app)
-// const analytics = getAnalytics(app)
